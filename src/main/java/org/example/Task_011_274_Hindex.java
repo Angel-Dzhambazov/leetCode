e17ee19531e6b3f490de7392e4c4b4dfc0e25f01
Angel Dzhambazov <angel.dzhambazov@gmail.com>
2025-10-27 12:15:01 +0200

    h intex solution

---
+++ src/main/java/org/example/Task_011_274_Hindex.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Task_011_274_Hindex {
+    public static void main(String[] args) {
+        int[] citations = {3,0,6,1,5};
+        System.out.println(hIndex(citations));
+    }
+    public static int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+
+        for (int i = 0; i < n; i++) {
+            int h = n - i; // number of papers with citations >= citations[i]
+            if (citations[i] >= h) {
+                return h;
+            }
+        }
+
+        return 0;
+    }
+}
