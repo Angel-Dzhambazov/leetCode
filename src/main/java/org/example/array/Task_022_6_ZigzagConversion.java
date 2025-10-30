e7200a8d02279fd2019157450ea2d1e7f6bdab1a
Angel Dzhambazov <angel.dzhambazov@gmail.com>
2025-10-30 10:41:26 +0200

    solution

--- src/main/java/org/example/array/Task_022_6_ZigzagConversion.java
+++ src/main/java/org/example/array/Task_022_6_ZigzagConversion.java
@@ -2,63 +2,52 @@ package org.example.array;
 
 public class Task_022_6_ZigzagConversion {
     public static void main(String[] args) {
-        String s = "ABCD";
-        int rows = 2;
+        String s = "ABCDEFGHIJKLMNOPqRstuvwxyz";
+        int rows = 5;
         System.out.println(convert(s, rows));
     }
 
     private static String convert(String s, int numRows) {
+        if (numRows == 1) return s;
         StringBuilder sb = new StringBuilder();
-
         int columns = s.length();
 
-
-        // int columns = (x + rows - 1) / rows;
         char[][] output = new char[numRows][columns];
         int r = 0;
         int c = 0;
         boolean down = true;
+
         for (int i = 0; i < s.length(); i++) {
+            output[r][c] = s.charAt(i);
+
             if (down) {
-                output[r][c] = s.charAt(i);
-                r++;   // r = 0 , 1, 2, 3
-                int toMoveUp = r % numRows;
-                if (toMoveUp == 0) {
+                r++;
+                if (r == numRows) {
                     down = false;
+                    r = numRows - 2;
                     c++;
-                    r -= 2;
-                    if (r < 0) {
-                        r = 0;
-                    }
                 }
             } else {
-                output[r][c] = s.charAt(i);
                 r--;
                 c++;
-                if (r <= 0) {
+                if (r < 0) {
                     down = true;
-                    r = 0;
+                    r = 1;
                 }
-
             }
         }
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < columns; j++) {
-
                 if (output[i][j] != 0) {
                     sb.append(output[i][j]);
                 }
-
-
             }
-
         }
         return sb.toString();
     }
 
     private static String convert2(String s, int rows) {
         StringBuilder sb = new StringBuilder();
-
 
         return sb.toString().trim();
 
