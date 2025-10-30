b029ee4a4485eeff684b2d4c0aa186e8468854fa
Angel Dzhambazov <angel.dzhambazov@gmail.com>
2025-10-30 11:15:25 +0200

    solution

---
+++ src/main/java/org/example/array/Task_023_38_FindIndexOfFirstOccurrence.java
@@ -0,0 +1,16 @@
+package org.example.array;
+
+public class Task_023_38_FindIndexOfFirstOccurrence {
+    public static void main(String[] args) {
+        String haystack = "leetcode";
+        String needle = "leeto";
+        System.out.println(strStr(haystack,needle));
+
+    }
+
+    private static int strStr(String haystack, String needle) {
+
+        int firstIndex = haystack.indexOf(needle);
+        return firstIndex;
+    }
+}
