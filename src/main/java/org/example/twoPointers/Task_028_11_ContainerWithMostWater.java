31ca1d43c8fd47953df5dff9e0a74c5835eac542
Angel Dzhambazov <angel.dzhambazov@gmail.com>
2025-11-03 09:15:04 +0200

    solution

---
+++ src/main/java/org/example/twoPointers/Task_028_11_ContainerWithMostWater.java
@@ -0,0 +1,17 @@
+package org.example.twoPointers;
+
+import java.util.Arrays;
+
+public class Task_028_11_ContainerWithMostWater {
+    public static void main(String[] args) {
+        int[] height = new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(maxArea(height));
+
+    }
+
+
+    private static int maxArea(int[] height) {
+        return height[0];
+
+    }
+}
